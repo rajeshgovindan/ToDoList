bfc48fbf81ef6cd3efdc26b8d729157d391e0a4c
rajeshGovindan <rajesh.govindan@gmail.com>
2014-07-04 23:04:07 +0530

    Save and Grid Display
    code to save the task and display it in Grid

---
+++ ToDoList/scripts/storage.js
@@ -0,0 +1,57 @@
+﻿
+function setItem(key, value) {
+    localStorage.setItem(key, JSON.stringify(value));
+};
+
+function getItem(key) {
+    return JSON.parse(localStorage.getItem(key), dateConvertor("dueDate","taskDate"));
+};
+
+
+function dateConvertor(keyNames)
+{
+    var args = Array.prototype.slice.call(arguments);
+    return function (k, v) {
+        if (args.indexOf(k) >= 0) {
+           var parsedDate = Date.parse(v);
+            //alert(parsedDate);
+            return new Date(parsedDate);
+        }
+        return v;
+    }
+}
+
+function taskStorage(key) {
+
+    var tasks = getItem(key);
+    if (tasks === null) {
+        tasks = [];
+    }
+    return {
+
+        Add: function (task) {
+            tasks.push(task);
+            setItem(key, tasks);
+        },
+        get: function () {
+            var tasks = getItem(key);
+            if (tasks === null) {
+                tasks = [];
+            };
+            return tasks;
+        }
+
+    };
+};
+
+
+//var taskStorage = {
+//    setItem: function (key, value) {
+//        localStorage.setItem(key, JSON.stringify(value));
+//    },
+//    getItem: function (key) {
+//        return JSON.parse(localStorage.getItem(key));
+//    },
+//    removeItem: function (key) {
+//    }
+//};
--- ToDoList/scripts/taskScript.js
+++ ToDoList/scripts/taskScript.js
@@ -1,4 +1,6 @@
 ﻿
+
+//Model
 function Task(name, description, dueDate) {
 
    // var taskId = this.generateId();
@@ -16,8 +18,19 @@ Task.prototype.generateId = function(){
     this.id = 1;
 };
 Task.prototype.Save = function(){
-    // code to save the task object in isolated storage.
+    var task = {id : this.id,
+        name : this.name,
+        description : this.description,
+        dueDate : this.dueDate,
+        taskDate : this.taskDate,
+        status : this.status
+    }
+    //taskStorage.setItem("ToDOTask", task);
+    var taskDb = taskStorage("task");
+    taskDb.Add(task);
 };
+
+//View
 var view = {
 
     readInputValue: function () {
@@ -29,24 +42,101 @@ var view = {
 
     },
     displayTasks: function (tasks) {
+        var taskGridTable = document.getElementById("taskGridTable");
+        
+        taskGridTable.innerHTML = grid.Header
+        for (var task in tasks) {
+            taskGridTable.innerHTML = taskGridTable.innerHTML + grid.row(tasks[task]);
+        }
+        //taskGridTable.innerHTML = grid.Header + rows;
     },
     displayMessage: function (message, type) {
+    },
+    validate: function () {
+
     }
 };
 
+//Controller
+function taskController() {
+    var dbStorage = taskStorage("task");
+    return {
 
+        getById: function (taskId) {
+        },
+        getAll: function () {
+            return dbStorage.get();
+        },
+        save: function (task) {
+        },
+        statusUpdate: function (taskId, status) {
+        },
+
+    };
+};
+
+
+//View_Events
+function init() {
+    var saveButton = document.getElementById("saveButton");
+    saveButton.onclick = saveButton_clicked;
+    var tasks = taskController().getAll();
+    view.displayTasks(tasks);
+};
+
+function saveButton_clicked(){
+    var task = view.readInputValue();
+    task.Save();
+    view.displayTasks(taskController().getAll());
+};
+
+
+var grid = {
+    Header : "<tr><th>TaskName</th><th>Date</th><th>Due on</th><th>Status</th></tr>",
+    row : function(task){
+        var row = "<tr><td>" + task.name + "</td>";
+        row = row + "<td>" + task.taskDate.toShortString() + "</td>";
+        row = row + "<td>" + task.dueDate.toShortString() + "</td>";
+        row = row + "<td>" + task.status + "</td>";
+        row = row + "</tr>";
+        return row ;
+    },
+
+};
+
+//Unit testing Methods
 function TestView_readInputvalue() {
 
     alert("called js function ");
-    console.log(view.readInputValue());
+    var task = view.readInputValue();
+    task.Save();
+
+};
+
+function TestView_displayTasks() {
+    var task = new Task("Task1", "Description 1", new Date("07/31/2014"));
+    var tasks = [];
+    tasks.push(task);
+
+    tasks.push(new Task("Task2", "Description 2", new Date("01/02/2015")));
+    view.displayTasks(tasks);
+};
+
+function TestModel_Task_Save() {
+    var task = new Task("Task3", "Description 3", new Date("07/31/2014"));
+    task.Save(task);
+};
+
+function TestController_GetAll() {
     
+   // var task = new Task("Task4", "Description 4", new Date("07/31/2014"));
+    var tasks = taskController().getAll();
+    view.displayTasks(tasks);
 };
-
-function init() {
-    var saveButton = document.getElementById("saveButton");
-    saveButton.onclick = TestView_readInputvalue;
-};
-
 
 
 window.onload = init;
+//TestView_displayTasks();
+//localStorage.removeItem("task");
+//TestModel_Task_Save();
+//TestController_GetAll();
